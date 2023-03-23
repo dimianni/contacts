8b029bd9b46895beb90d37d02778499672c8e255
Dmytro Anikin <dimianni@users.noreply.github.com>
2023-03-23 19:56:21 +0100

    path updates4

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <div className="container mx-auto max-w-2xl">
         <div className="head w-full flex flex-col justify-center items-center">
           <div className="myimg w-24 h-24 rounded-full overflow-hidden mb-4">
-            <img src="/assets/dimianni.jpg" className="w-full h-auto" alt="Dmytro Anikin. Dimianni." />
+            <img src="assets/dimianni.jpg" className="w-full h-auto" alt="Dmytro Anikin. Dimianni." />
           </div>
           <div className="name">
             <h1 className='text-xl font-bold'>Dmytro Anikin</h1>
@@ -34,7 +34,7 @@ function App() {
                 <div key={i} className="link mb-4">
                   <a href="#" target="_blank" rel="noopener" className='flex justify-center items-center bg-light-grey rounded px-5 py-4 relative'>
                     <div className="icon absolute top-1/2 -translate-y-2/4 left-2 w-12 h-12">
-                      <img src={`/assets/${social.icon}`} alt="" className='w-full h-auto object-contain' />
+                      <img src={`assets/${social.icon}`} alt="" className='w-full h-auto object-contain' />
                     </div>
                     <p className='w-full font-medium text-base text-center'>{social.name}</p>
                   </a>
